058a838ee7fc8810ee017037b4aafa8d44214d25
Francesco Mancini <37000331+francesco1997@users.noreply.github.com>
2020-03-12 17:28:36 +0100

    Created HelloWorld class

---
+++ it/uniroma2/ing/isw2/travisLaboratory/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package it.uniroma2.ing.isw2.travisLaboratory;
+
+/**
+ * @author fmancini
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+
+	}
+
+}
